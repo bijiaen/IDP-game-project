082a7192d6356458b7088076afae6c9ff6fd6825
bijiaen <77262000+bijiaen@users.noreply.github.com>
2022-01-26 21:38:48 +0800

    score panel

--- Unity Basics/Assets/Scripts/ScorePanel.cs
+++ Unity Basics/Assets/Scripts/ScorePanel.cs
@@ -6,22 +6,20 @@ using UnityEngine.UI;
 public class ScorePanel : MonoBehaviour
 {
     public Text levelCoinsTextElement;
-    //public Text totalCoinsTextElement;
+    public Text totalCoinsTextElement;
 
     private int lCoin;
-    //private int tCoin;
+    private int tCoin;
 
     
     // Start is called before the first frame update
     void Start()
     {
+        tCoin = PlayerPrefs.GetInt("totalcoins", 15);
         lCoin = PlayerPrefs.GetInt("levelcoins", 0);
-        //tCoin = PlayerPrefs.GetInt("totalcoins", 0);
-
-        //Debug.Log("IN  GAMEOVER" + tCoin);
 
         levelCoinsTextElement.text = "LEVEL COINS: " + lCoin.ToString();
-        //totalCoinsTextElement.text = "TOTAL COINS: " + tCoin.ToString();
+        totalCoinsTextElement.text = "TOTAL COINS: " + tCoin.ToString();
     }
 
     // Update is called once per frame
