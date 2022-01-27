acc31318259b9c6c0458d9ab9ad22e216a2cb86d
bijiaen <77262000+bijiaen@users.noreply.github.com>
2022-01-28 00:07:37 +0800

    disabled harder levels for player

--- Unity Basics/Assets/Scripts/MainMenu.cs
+++ Unity Basics/Assets/Scripts/MainMenu.cs
@@ -8,13 +8,44 @@ public class MainMenu : MonoBehaviour
     public GameObject MainMenuElement;
     public GameObject LevelMenuElement;
     public GameObject OptionsMenuElement;
+    public GameObject Level1Element;
+    public GameObject Level2Element;
+    public GameObject Level3Element;
 
     private int isLevelSelect;
+    private int highestLevel;
 
-    void Start() 
+    void Start()
     {
+
+        if (!PlayerPrefs.HasKey("highestLevel"))
+        {
+            PlayerPrefs.SetInt("highestLevel", 0);
+            Debug.Log("first time player");
+            Level1Element.SetActive(true);
+        }
+        else
+        {
+            highestLevel = PlayerPrefs.GetInt("highestLevel");
+            Debug.Log("highestLevel: " + highestLevel.ToString());
+
+            if (highestLevel >= 0)
+            {
+                Level1Element.SetActive(true);
+            }
+            if (highestLevel >= 1)
+            {
+                Level2Element.SetActive(true);
+            }
+            if (highestLevel >= 2)
+            {
+                Level3Element.SetActive(true);
+            }
+        }
+        //If the previous scene is Finish, and i click "level select", isLevelSelect = 1
         isLevelSelect = PlayerPrefs.GetInt("isLevelSelect", 0);
-        if (isLevelSelect == 1) {
+        if (isLevelSelect == 1)
+        {
             LevelMenuElement.SetActive(true);
             MainMenuElement.SetActive(false);
             OptionsMenuElement.SetActive(false);
@@ -23,7 +54,7 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-   
+
 
     // public void PlayGame()
     // {
@@ -36,17 +67,20 @@ public class MainMenu : MonoBehaviour
         //Application.Quit();
     }
 
-    public void PlayL1() {
+    public void PlayL1()
+    {
         PlayerPrefs.SetInt("currentLevel", 1);
         SceneManager.LoadScene("Level1");
     }
 
-    public void PlayL2() {
+    public void PlayL2()
+    {
         PlayerPrefs.SetInt("currentLevel", 2);
         SceneManager.LoadScene("Level2");
     }
 
-    public void PlayL3() {
+    public void PlayL3()
+    {
         PlayerPrefs.SetInt("currentLevel", 3);
         SceneManager.LoadScene("Level3");
     }
--- Unity Basics/Assets/Scripts/restart.cs
+++ Unity Basics/Assets/Scripts/restart.cs
@@ -7,27 +7,39 @@ public class restart : MonoBehaviour
 {
 
     private int currentLevel;
+    private int highestLevel;
     // Start is called before the first frame update
     void Start()
     {
         currentLevel = PlayerPrefs.GetInt("currentLevel");
+        highestLevel = PlayerPrefs.GetInt("highestLevel");
+
+        if (currentLevel > highestLevel)
+        {
+            PlayerPrefs.SetInt("highestLevel", currentLevel);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void LoadGame()
     {
-        if (currentLevel == 1) {
+        if (currentLevel == 1)
+        {
             SceneManager.LoadScene("Level1");
-        } else if (currentLevel == 2) {
+        }
+        else if (currentLevel == 2)
+        {
             SceneManager.LoadScene("Level2");
-        } else if (currentLevel == 3) {
+        }
+        else if (currentLevel == 3)
+        {
             SceneManager.LoadScene("Level3");
-        } 
+        }
     }
 
     public void LoadMenu()
@@ -35,7 +47,8 @@ public class restart : MonoBehaviour
         SceneManager.LoadScene("Menu");
     }
 
-    public void LoadLevelSelection() {
+    public void LoadLevelSelection()
+    {
         PlayerPrefs.SetInt("isLevelSelect", 1);
         SceneManager.LoadScene("Menu");
 
